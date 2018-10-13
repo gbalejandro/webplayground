198505b3a5d260f8552c6c93d597a5a5532ea51a
gbalejandro <alejandrobright@gmail.com>
2018-10-13 12:24:48 -0500

    Se crea la primera parte de los perfiles

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Auth redirects
-LOGIN_REDIRECT_URL = 'pages:pages'
+# LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
 
 # Emails
@@ -134,3 +134,7 @@ if DEBUG:
 else:
     # Aquí hay que configurar un email real para producción
     pass
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

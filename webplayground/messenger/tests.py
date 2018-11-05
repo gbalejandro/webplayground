265052d631a8063fd009a7ba973cf41f91474fb7
Alejandro Gonzalez Ballesteros <alejandrobright@gmail.com>
2018-11-05 15:33:34 -0500

    Se agregan pruebas unitarias con TDD.

--- webplayground/messenger/tests.py
+++ webplayground/messenger/tests.py
@@ -7,6 +7,7 @@ class ThreadTestCase(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user('user1', None, 'test1234')
         self.user2 = User.objects.create_user('user2', None, 'test1234')
+        self.user3 = User.objects.create_user('user3', None, 'test1234')
 
         self.thread = Thread.objects.create()
 
@@ -32,3 +33,23 @@ class ThreadTestCase(TestCase):
 
         for message in self.thread.messages.all():
             print("({}): {}".format(message.user, message.content))
+
+    def test_add_message_from_user_not_in_thread(self):
+        self.thread.users.add(self.user1, self.user2)
+        message1 = Message.objects.create(user=self.user1, content="Muy buenas")
+        message2 = Message.objects.create(user=self.user2, content="Hola")
+        message3 = Message.objects.create(user=self.user3, content="Soy un espía")
+        self.thread.messages.add(message1, message2, message3)
+        self.assertEqual(len(self.thread.messages.all()), 2)
+
+    def test_find_thread_with_custom_manager(self):
+        self.thread.users.add(self.user1, self.user2)
+        thread = Thread.objects.find(self.user1,self.user2)
+        self.assertEqual(self.thread, thread)
+
+    def test_find_or_create_thread_with_custom_manager(self):
+        self.thread.users.add(self.user1, self.user2)
+        thread = Thread.objects.find_or_create(self.user1,self.user2)
+        self.assertEqual(self.thread, thread)
+        thread = Thread.objects.find_or_create(self.user1,self.user2)
+        self.assertIsNotNone(thread)

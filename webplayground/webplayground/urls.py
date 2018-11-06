def1f7b04f94fb0e0f29c92574e803fc5351a98b
Alejandro Gonzalez Ballesteros <alejandrobright@gmail.com>
2018-11-06 15:46:27 -0500

    Se agregan vistas para recuperar conversaciones

--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
+from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -25,6 +27,11 @@ urlpatterns = [
     # Paths de Auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
+    # Paths de profiles
+    path('profiles/', include(profiles_patterns)),
+    # Paths de Messenger
+    path('messenger/', include(messenger_patterns)),
+
 ]
 
 if settings.DEBUG:

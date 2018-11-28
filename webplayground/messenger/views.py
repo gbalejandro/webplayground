4a71e71e48beef4a7999cda72674b2a9ed0af03a
Alejandro Gonzalez Ballesteros <alejandrobright@gmail.com>
2018-11-28 08:38:18 -0500

    Se agregó código JavaScript para las peticiones asíncronas del mensajero.

--- webplayground/messenger/views.py
+++ webplayground/messenger/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
-from .models import Thread
+from .models import Thread, Message
 from django.http import Http404, JsonResponse
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
 
 @method_decorator(login_required, name="dispatch")
 class ThreadList(TemplateView):
@@ -24,4 +25,14 @@ class ThreadDetail(DetailView):
 
 def add_message(request, pk):
     json_response = {'created':False}
+    if request.user.is_authenticated:
+        content = request.GET.get('content', None)
+        if content:
+            thread = get_object_or_404(Thread, pk=pk)
+            message = Message.objects.create(user=request.user, content=content)
+            thread.messages.add(message)
+            json_response['created'] = True
+    else:
+        raise Http404('User is not authenticated')
+
     return JsonResponse(json_response)
